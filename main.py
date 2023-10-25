ff9b18365b102fc8ba4557a885faaa53e66ae6f0
Daniel ATTITSOGBE <daniel.attitsogbe02@gmail.com>
2023-10-25 21:50:08 +0000

    Update api docs | Add tags and schemas

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
-from utils.read_demography import read_population
-from utils.read_geography import read_location, read_subdivisions
+from models import Census, Location, Country
+from utils import read_population, read_location, read_subdivisions
+
 
 app = FastAPI()
+
+
+class Tags(Enum):
+    demography = "demography"
+    geography = "geography"
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/demography/population")
+@app.get("/demography/population", tags=[Tags.demography], response_model=Census)
 def get_population():
     population = read_population()
     return population
 
-@app.get("/geography/location")
+@app.get("/geography/location", tags=[Tags.geography], response_model=Location)
 def get_location():
     location = read_location()
     return location
 
-@app.get("/geography/subdivisions")
+@app.get("/geography/subdivisions", tags=[Tags.geography], response_model=Country)
 def get_subdivisions():
     subdivisions = read_subdivisions()
     return subdivisions
---
+++ utils/__init__.py
@@ -0,0 +1,2 @@
+from .read_demography import read_population
+from .read_geography import read_location, read_subdivisions
